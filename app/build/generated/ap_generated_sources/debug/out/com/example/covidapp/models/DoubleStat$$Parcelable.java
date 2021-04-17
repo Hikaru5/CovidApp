6e4e5b2ac9ae07144810c7fe8795f6f354a56f7f
curtisthepotato <curtisktaylorrrr@gmail.com>
2021-04-17 10:18:34 -0700

    minor changes

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/covidapp/models/DoubleStat$$Parcelable.java
@@ -0,0 +1,92 @@
+
+package com.example.covidapp.models;
+
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+import org.parceler.Generated;
+import org.parceler.IdentityCollection;
+import org.parceler.ParcelWrapper;
+import org.parceler.ParcelerRuntimeException;
+
+@Generated("org.parceler.ParcelAnnotationProcessor")
+@SuppressWarnings({
+    "unchecked",
+    "deprecation"
+})
+public class DoubleStat$$Parcelable
+    implements Parcelable, ParcelWrapper<com.example.covidapp.models.DoubleStat>
+{
+
+    private com.example.covidapp.models.DoubleStat doubleStat$$0;
+    @SuppressWarnings("UnusedDeclaration")
+    public final static Creator<DoubleStat$$Parcelable>CREATOR = new Creator<DoubleStat$$Parcelable>() {
+
+
+        @Override
+        public DoubleStat$$Parcelable createFromParcel(android.os.Parcel parcel$$2) {
+            return new DoubleStat$$Parcelable(read(parcel$$2, new IdentityCollection()));
+        }
+
+        @Override
+        public DoubleStat$$Parcelable[] newArray(int size) {
+            return new DoubleStat$$Parcelable[size] ;
+        }
+
+    }
+    ;
+
+    public DoubleStat$$Parcelable(com.example.covidapp.models.DoubleStat doubleStat$$2) {
+        doubleStat$$0 = doubleStat$$2;
+    }
+
+    @Override
+    public void writeToParcel(android.os.Parcel parcel$$0, int flags) {
+        write(doubleStat$$0, parcel$$0, flags, new IdentityCollection());
+    }
+
+    public static void write(com.example.covidapp.models.DoubleStat doubleStat$$1, android.os.Parcel parcel$$1, int flags$$0, IdentityCollection identityMap$$0) {
+        int identity$$0 = identityMap$$0 .getKey(doubleStat$$1);
+        if (identity$$0 != -1) {
+            parcel$$1 .writeInt(identity$$0);
+        } else {
+            parcel$$1 .writeInt(identityMap$$0 .put(doubleStat$$1));
+            parcel$$1 .writeString(doubleStat$$1 .statTitle2);
+            parcel$$1 .writeInt(doubleStat$$1 .stat2);
+            parcel$$1 .writeString(doubleStat$$1 .statTitle1);
+            parcel$$1 .writeInt(doubleStat$$1 .stat1);
+        }
+    }
+
+    @Override
+    public int describeContents() {
+        return  0;
+    }
+
+    @Override
+    public com.example.covidapp.models.DoubleStat getParcel() {
+        return doubleStat$$0;
+    }
+
+    public static com.example.covidapp.models.DoubleStat read(android.os.Parcel parcel$$3, IdentityCollection identityMap$$1) {
+        int identity$$1 = parcel$$3 .readInt();
+        if (identityMap$$1 .containsKey(identity$$1)) {
+            if (identityMap$$1 .isReserved(identity$$1)) {
+                throw new ParcelerRuntimeException("An instance loop was detected whild building Parcelable and deseralization cannot continue.  This error is most likely due to using @ParcelConstructor or @ParcelFactory.");
+            }
+            return identityMap$$1 .get(identity$$1);
+        } else {
+            com.example.covidapp.models.DoubleStat doubleStat$$4;
+            int reservation$$0 = identityMap$$1 .reserve();
+            doubleStat$$4 = new com.example.covidapp.models.DoubleStat();
+            identityMap$$1 .put(reservation$$0, doubleStat$$4);
+            doubleStat$$4 .statTitle2 = parcel$$3 .readString();
+            doubleStat$$4 .stat2 = parcel$$3 .readInt();
+            doubleStat$$4 .statTitle1 = parcel$$3 .readString();
+            doubleStat$$4 .stat1 = parcel$$3 .readInt();
+            com.example.covidapp.models.DoubleStat doubleStat$$3 = doubleStat$$4;
+            identityMap$$1 .put(identity$$1, doubleStat$$3);
+            return doubleStat$$3;
+        }
+    }
+
+}
---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/covidapp/models/State$$Parcelable.java
@@ -0,0 +1,94 @@
+
+package com.example.covidapp.models;
+
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+import org.parceler.Generated;
+import org.parceler.IdentityCollection;
+import org.parceler.ParcelWrapper;
+import org.parceler.ParcelerRuntimeException;
+
+@Generated("org.parceler.ParcelAnnotationProcessor")
+@SuppressWarnings({
+    "unchecked",
+    "deprecation"
+})
+public class State$$Parcelable
+    implements Parcelable, ParcelWrapper<com.example.covidapp.models.State>
+{
+
+    private com.example.covidapp.models.State state$$0;
+    @SuppressWarnings("UnusedDeclaration")
+    public final static Creator<State$$Parcelable>CREATOR = new Creator<State$$Parcelable>() {
+
+
+        @Override
+        public State$$Parcelable createFromParcel(android.os.Parcel parcel$$2) {
+            return new State$$Parcelable(read(parcel$$2, new IdentityCollection()));
+        }
+
+        @Override
+        public State$$Parcelable[] newArray(int size) {
+            return new State$$Parcelable[size] ;
+        }
+
+    }
+    ;
+
+    public State$$Parcelable(com.example.covidapp.models.State state$$2) {
+        state$$0 = state$$2;
+    }
+
+    @Override
+    public void writeToParcel(android.os.Parcel parcel$$0, int flags) {
+        write(state$$0, parcel$$0, flags, new IdentityCollection());
+    }
+
+    public static void write(com.example.covidapp.models.State state$$1, android.os.Parcel parcel$$1, int flags$$0, IdentityCollection identityMap$$0) {
+        int identity$$0 = identityMap$$0 .getKey(state$$1);
+        if (identity$$0 != -1) {
+            parcel$$1 .writeInt(identity$$0);
+        } else {
+            parcel$$1 .writeInt(identityMap$$0 .put(state$$1));
+            parcel$$1 .writeInt(state$$1 .infectedCount);
+            parcel$$1 .writeString(state$$1 .stateName);
+            parcel$$1 .writeInt(state$$1 .deathCount);
+            parcel$$1 .writeInt(state$$1 .recoveredCount);
+            parcel$$1 .writeString(state$$1 .stateImage);
+        }
+    }
+
+    @Override
+    public int describeContents() {
+        return  0;
+    }
+
+    @Override
+    public com.example.covidapp.models.State getParcel() {
+        return state$$0;
+    }
+
+    public static com.example.covidapp.models.State read(android.os.Parcel parcel$$3, IdentityCollection identityMap$$1) {
+        int identity$$1 = parcel$$3 .readInt();
+        if (identityMap$$1 .containsKey(identity$$1)) {
+            if (identityMap$$1 .isReserved(identity$$1)) {
+                throw new ParcelerRuntimeException("An instance loop was detected whild building Parcelable and deseralization cannot continue.  This error is most likely due to using @ParcelConstructor or @ParcelFactory.");
+            }
+            return identityMap$$1 .get(identity$$1);
+        } else {
+            com.example.covidapp.models.State state$$4;
+            int reservation$$0 = identityMap$$1 .reserve();
+            state$$4 = new com.example.covidapp.models.State();
+            identityMap$$1 .put(reservation$$0, state$$4);
+            state$$4 .infectedCount = parcel$$3 .readInt();
+            state$$4 .stateName = parcel$$3 .readString();
+            state$$4 .deathCount = parcel$$3 .readInt();
+            state$$4 .recoveredCount = parcel$$3 .readInt();
+            state$$4 .stateImage = parcel$$3 .readString();
+            com.example.covidapp.models.State state$$3 = state$$4;
+            identityMap$$1 .put(identity$$1, state$$3);
+            return state$$3;
+        }
+    }
+
+}
